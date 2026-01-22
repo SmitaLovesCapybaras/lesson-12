f17381fcb8fd21bb91f241092298227e12f12173
SmitaLovesCapybaras <Baggiapari@gmail.com>
2026-01-21 19:40:31 -0500

    Add files via upload

---
+++ Character occurance.py
@@ -0,0 +1,9 @@
+string=input("Enter your own word:")
+char=input("Enter you character:")
+i=0
+count=0
+while(i<len(string)):
+    if(string[i]==char):
+        count=count+1
+    i=i+1
+print("The total number of times",char,"has occured=", count)
